a996ed5c216db277382f9eb41ea88d66b3928cee
thomas bush <thomasbush52@gmail.com>
2025-07-10 16:20:23 -0400

    reclycing embedder

--- tutorials/feature_embedding/recycling_embedder.py
+++ tutorials/feature_embedding/recycling_embedder.py
@@ -5,7 +5,7 @@ class RecyclingEmbedder(nn.Module):
     """
     Implements Algorithm 32.
     """
-    
+
     def __init__(self, c_m, c_z):
         """
         Initializes the RecyclingEmbedder.
@@ -24,12 +24,14 @@ class RecyclingEmbedder(nn.Module):
         ##########################################################################
 
         # Replace "pass" statement with your code
-        pass
+        self.layer_norm_m = nn.LayerNorm(c_m)
+        self.layer_norm_z = nn.LayerNorm(c_z)
+        self.linear = nn.Linear(self.bin_count, c_z)
 
         ##########################################################################
         # END OF YOUR CODE                                                       #
         ##########################################################################
-    
+
     def forward(self, m_prev, z_prev, x_prev):
         """
         Forward pass for Algorithm 32.
@@ -37,13 +39,13 @@ class RecyclingEmbedder(nn.Module):
         Args:
             m_prev (torch.tensor): MSA representation of previous iteration, shape (*, N_seq, N_res, c_m).
             z_prev (torch.tensor): Pair representation of previous iteration, shape (*, N_res, N_res, c_z).
-            x_prev (torch.tensor): Pseudo-beta positions from the previous iterations of 
-                shape (*, N_res, 3). These are the positions of the C-beta atoms from the 
+            x_prev (torch.tensor): Pseudo-beta positions from the previous iterations of
+                shape (*, N_res, 3). These are the positions of the C-beta atoms from the
                 last prediction (in Angstrom), or of C-alpha for glycin.
-            
+
 
         Returns:
-            tuple: A tuple consisting of m_out of shape (*, N_res, c_m) and z_out 
+            tuple: A tuple consisting of m_out of shape (*, N_res, c_m) and z_out
                 of shape (*, N_res, N_res, c_z).
         """
 
@@ -71,8 +73,22 @@ class RecyclingEmbedder(nn.Module):
         #   lowest bin, but to no class at all.                                  #
         ##########################################################################
 
-        # Replace "pass" statement with your code
-        pass
+        # Replace "pass" statement with yo
+        d = torch.linalg.vector_norm(x_prev.unsqueeze(-2) - x_prev.unsqueeze(-3), dim=-1)
+
+        bins_lower = torch.linspace(self.bin_start, self.bin_end, self.bin_count, device=x_prev.device)
+        bins_upper = torch.cat((bins_lower[1:], torch.tensor([1e8], device=x_prev.device)))
+
+        d = d.unsqueeze(-1)
+        d = ((d>bins_lower) * (d<bins_upper)).type(x_prev.dtype)
+        d = self.linear(d)
+        z_out = d + self.layer_norm_z(z_prev)
+        m_out = self.layer_norm_m(m_prev[..., 0, :, :])
+
+
+
+
+
 
         ##########################################################################
         # END OF YOUR CODE                                                       #
